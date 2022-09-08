bf4a142f96ef8ca88474920471bcb029a021497a
Mik135 <michaelsavior1@gmail.com>
2022-09-08 16:51:22 -0700

    Adding a return value

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,4 +18,5 @@ int main()
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));;
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
