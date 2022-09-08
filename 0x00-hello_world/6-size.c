56eb724e5c782310c65e9ce80529cf2f85d3c908
Mik135 <michaelsavior1@gmail.com>
2022-09-08 16:39:49 -0700

    Rough code

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+/*
+*File: 6-size.c
+*
+* Author: Michael Michael Saviour
+*/
+
+#include <stdio.h>
+
+/** main - prints the size of various types on the computer it is compiled and run on.
+*
+* Return: 0.
+*/
+
+int main()
+{
+	printf("Size of a char: %d byte( s )\n", sizeof(char));
+	printf("Size of an int: %d byte( s )\n", sizeof(int));
+	printf("Size of a long int: %d byte( s )\n", sizeof(long int));
+	printf("Size of a float: %d byte( s )\n", sizeof(float));
+}
