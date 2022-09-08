7813a2c9920b1d9983a00dd8d0ee736db1c5f88e
Mik135 <michaelsavior1@gmail.com>
2022-09-08 15:56:09 -0700

    Print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-/* Print a string using the puts function */
+/**
+*Print a string using the puts function
+*/
 int main(void)
 {
 	char str1[] = "\"Programming is like building a multilingual puzzle";
