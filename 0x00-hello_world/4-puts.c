8a6f51f59d9a73e91bd3d83bf31e3adde5fb8817
Mik135 <michaelsavior1@gmail.com>
2022-09-08 16:02:02 -0700

    Print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
-*Print a string using the puts function
+*Print "Programming is like building a multilingual puzzle,followed by a new line.
+*
+* Returm: Always 0.
 */
 int main(void)
 {
