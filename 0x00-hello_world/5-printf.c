4db0996a50c03c8ccf5c8738dc1e43403cfb4abd
Mik135 <michaelsavior1@gmail.com>
2022-09-08 17:49:21 -0700

    Yes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,8 @@
 #include <stdio.h>
 
 /**
-*main -  prints with proper grammar, but the outcome
-*is a piece of art, followed by a new line.
+*main -  prints "with proper grammar, but the outcome
+*is a piece of art," followed by a new line.
 *
 *Return: 0.
 */
