a590d6ac5a4a2fdae028327e343e1388d7950b9c
Mik135 <michaelsavior1@gmail.com>
2022-09-09 16:42:21 -0700

    Lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*
+* File: 2-print-alphabet.c
+*
+* Author: mik135
+*/
+
+/**
+* main - prints the alphabet in lowercase
+* followed by a new line
+*
+* Return: 0
+*/
+int main(void)
+{
+	char n;
+	n = 'a';
+
+	while (n <= 'z') {
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
