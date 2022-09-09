afd307a75d0ac07bdbdebad0e1302d1919ce4173
Mik135 <michaelsavior1@gmail.com>
2022-09-09 15:30:18 -0700

    Using if statements

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/*
+* File: 0-positive_or_negative.c
+*
+* Author: mik135
+*/
+
+/**
+* main - Assigns a random number to n everytime 
+* it is executed
+*
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n > 0)
+		printf("%d is positive\n", n);
+	else
+		printf("%d is negative\n", n);
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This directory contains code related to program flow control, conditionals and assignment operators
