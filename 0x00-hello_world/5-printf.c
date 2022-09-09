98165b964a5fce82aad1a154ae5797001c434ec5
Mik135 <michaelsavior1@gmail.com>
2022-09-08 17:24:26 -0700

    More commits

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,10 +6,10 @@
 #include <stdio.h>
 
 /**
-* main - Prints with proper grammar, but the outcome
+* main: Prints with proper grammar, but the outcome
 * is a piece of art, followed by a new line.
 *
-* Return: Always 0.
+* Return: 0.
 */
 
 int main(void)
