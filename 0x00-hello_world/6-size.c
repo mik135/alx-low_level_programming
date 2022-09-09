f66c9eb73c64d20b170d0523e1ab761c73f79bec
Mik135 <michaelsavior1@gmail.com>
2022-09-08 17:06:20 -0700

    Sizes of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,8 @@
 
 #include <stdio.h>
 
-/** main - prints the size of various types on the computer it is compiled and run on.
+/**
+*main - prints the size of various types on the computer it is compiled and run on.
 *
 * Return: 0.
 */
