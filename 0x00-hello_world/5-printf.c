b985576b234cd669b3aa000f20ecf1d19763f217
Mik135 <michaelsavior1@gmail.com>
2022-09-08 17:27:56 -0700

    Sense

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 /**
-* File: 5-printf.c
-* Author: Michael Michael Saviour
+*File: 5-printf.c
+*Author: Michael Michael Saviour
 */
 
 #include <stdio.h>
 
 /**
-* main: Prints with proper grammar, but the outcome
-* is a piece of art, followed by a new line.
+*main: Prints with proper grammar, but the outcome
+*is a piece of art, followed by a new line.
 *
-* Return: 0.
+*Return: 0.
 */
 
 int main(void)
