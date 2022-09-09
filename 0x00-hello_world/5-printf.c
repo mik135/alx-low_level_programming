698e98465b37e871369c5e177d4ac1ab888a5bda
Mik135 <michaelsavior1@gmail.com>
2022-09-08 17:17:49 -0700

    Solve another problem

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,17 @@
 /** 
-*File: 5-printf.c
+* File: 5-printf.c
 * Author: Michael Michael Saviour
 */
 
 #include <stdio.h>
 
-/** main - Prints with proper grammar, but the outcome
-*is a piece of art, followed by a new line.
+/**
+* main - Prints with proper grammar, but the outcome
+* is a piece of art, followed by a new line.
 *
 * Return: Always 0.
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
