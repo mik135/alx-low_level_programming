a9c6d4b5e8ed078a81da591bb34c037878ae574d
Mik135 <michaelsavior1@gmail.com>
2022-09-09 17:20:57 -0700

    Working with numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/* 
+* Files: 5-print_numbers.c
+*
+* Author: mik135
+*/
+
+/**
+* main - prints all single digit numbers 
+* of base 10 starting from 0
+* followed by a new line
+*
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 9)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+
+	return (0);
+}
+	
