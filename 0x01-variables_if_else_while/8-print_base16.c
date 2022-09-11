72217c781afe6476dffa7a7d1f855c5778b74d2d
Mik135 <michaelsavior1@gmail.com>
2022-09-11 16:08:09 -0700

    Source code to print hex numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/*
+* File: 8-print_base16.c
+*
+* Author: mik135
+*/
+
+/**
+* main - prints all numbers of base 16 in lowercase
+* followed by a new line
+*
+* Return: 0
+*/
+int main(void)
+{
+	char num;
+
+	char hex;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	for (hex = 'a'; hex <= 'f'; hex++)
+	{
+		putchar(hex);
+	}
+
+	putchar('\n');
+	return(0);
+}
