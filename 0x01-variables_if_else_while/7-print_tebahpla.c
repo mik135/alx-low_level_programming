b930c7add377a782608632210d074fb8fbc19e15
Mik135 <michaelsavior1@gmail.com>
2022-09-11 15:24:41 -0700

    Source code to print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+* File: 7-print_tebahpla.c
+*
+* Author: mik135;
+*/
+
+/**
+* main - print the lowercase alphabet in reverse
+* followed by a new line
+*
+* Return: 0;
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return 0;
+}
