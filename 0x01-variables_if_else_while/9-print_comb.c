815d8cd0537402d183af2d643cda12e636afec94
Mik135 <michaelsavior1@gmail.com>
2022-09-12 13:37:24 -0700

    Fixing more bugs in file 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,14 +15,16 @@ int main(void)
 
 	for (num = 0; num <= 9; num++)
 	{
+		putchar((num % 10) +'0');
+
 		if (num == 9)
 		{
 			continue;
 		}
-		putchar((num % 10) + '0');
 		putchar(',');
 		putchar(' ');
 	}
 
+	putchar('\n');
 	return (0);
 }
