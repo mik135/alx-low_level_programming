3278f5b736976d994b011cac152c48064ac51ac3
Mik135 <michaelsavior1@gmail.com>
2022-09-12 16:48:38 -0700

    Combinatorucs of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/*
+* File: 100-print_comb3.c
+*
+* Author: mik135
+*/
+
+/**
+* main - prints all possible different combinations
+* of two digits
+*
+* Return: 0
+*/
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			if (digit1 != digit2 && digit2 != digit1)
+			{
+				putchar(digit1 + '0');
+				putchar(digit2 + '0');
+			}
+			if (digit1 == 8 && digit2 == 9)
+			{
+				continue;
+			}
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
