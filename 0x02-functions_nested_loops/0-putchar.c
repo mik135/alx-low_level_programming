c8fe78a86859e34d132870988e04d97316f4f25e
Mik135 <michaelsavior1@gmail.com>
2022-09-13 13:46:56 -0700

    Using the putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*
+* File: 0-putchar.c
+*
+* Author: mik135
+*/
+
+/**
+* main - prints _putchar
+* followed by a new line
+*
+* Return: 0
+*/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains source codes illustrating the use of functions and nested loops in C programming language
