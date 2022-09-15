11c79d77ba5bac8a649ec8d4235fab7a9a8d55e0
Mik135 <michaelsavior1@gmail.com>
2022-09-15 15:26:05 -0700

    Last Hello World task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* File: 101-quote.c
+*
+* Author: mik135
+*/
+
+/**
+* main - prints a string
+*
+* Return: 0
+*/
+int main(void)
+{
+	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	for (int count = 0; count < 100; count++)
+	{
+		if (string[count] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		else
+			putchar(string[count]);
+	}
+	return (0);
+}	
