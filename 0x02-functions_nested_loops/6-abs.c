726695ddf315ff8489b243577638e03f6be0d681
Mik135 <michaelsavior1@gmail.com>
2022-09-19 09:18:19 -0700

    Function that prints only absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _abs - computes the absolute value of an interger
+* @i: Interger value to be computed
+* Return: 0
+*/
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = i * -1;
+	}
+	return (i);
+}
