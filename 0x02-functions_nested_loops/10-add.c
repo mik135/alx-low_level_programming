938cfd2b9d15fe566f9ef7de6a8c3e45193be2b6
Mik135 <michaelsavior1@gmail.com>
2022-09-19 10:26:58 -0700

    Fixed in a bug in file 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
 * add - adds two intergers and returns the result
-* @a - first number to be added
-* @b - second number to be added
+* @a: first number to be added
+* @b: second number to be added
+* Return: sum of two numbers
 */
 int add(int a, int b)
 {
