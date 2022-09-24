de8009e0d2597099288b808cd342a8ca541d47a6
Mik135 <michaelsavior1@gmail.com>
2022-09-24 08:22:59 -0700

    Fibonacci sequence

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - program that prints the first 50 numbers of the fibonacci sequence
+*
+* Return: 0;
+*/
+int main(void)
+{
+	int term1 = 1, term2 = 2;
+
+	int count, nextnum;
+
+	count = 50;
+
+	printf("%d, %d, ", term1, term2);
+
+	for (int j = 2; j < 49; j++)
+	{
+		nextnum = term1 + term2;
+		term1 = term2;
+		term2 = nextnum;
+		printf("%d, ", nextnum);
+	}
+
+	printf("\n");
+	return (0);
+}
